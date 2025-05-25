12c971176118e7b4eb9c107c9a4cf41a60746ef0
zengfanxing <zengfanxing@shopline.com>
2025-05-25 15:29:45 +0800

    fix: 优化配置逻辑

--- scripts/core/common_plugin.core.js
+++ scripts/core/common_plugin.core.js
@@ -1,5 +1,5 @@
 window.common_plugin_core = async () => {
-    console.log('common_plugin_core running', '202505212347')
+    console.log('common_plugin_core running', '2025052515429')
     const matchDomains = ['www.gigab2b.com', 'www.saleyee.cn', 'www.temu.com', 'xhl.topwms.com', 'us.goodcang.com', 'returnhelper.com', 'oms.xlwms.com']
     // 一下内容在指定域名下生效
     if (!matchDomains.includes(window.location.host)) {return}
