23b52fa6ae208bbe6f84956b448c1d6e434426ce
zengfanxing <zengfanxing@joyy.com>
2025-06-11 14:23:25 +0800

    feat: 优化同步自动结算

--- scripts/core/temu_helper_v2.core.js
+++ scripts/core/temu_helper_v2.core.js
@@ -1,5 +1,5 @@
 window.temu_helper_v2_core = async () => {
-    console.log('temu_helper_v2_core running', '202506102351')
+    console.log('temu_helper_v2_core running', '202506111422')
     if (window.self !== window.top || window.location.pathname === '/mmsos/print.html') return
     let mallId = window.rawData?.store?.mallid || window.localStorage.getItem('mall-info-id') || window.localStorage.getItem('agentseller-mall-info-id') || window.localStorage.getItem('dxmManualCrawlMallId')
     try {
@@ -1633,7 +1633,7 @@ window.temu_helper_v2_core = async () => {
                     } catch(err) {}
                 }
             }
-            if (!AutoDisabled) {
+            if (window.location.host.includes('agentseller-us.temu.com') && !AutoDisabled) {
                 autoSyncCheckout();
                 setExactInterval(async () => {
                     autoSyncCheckout();
