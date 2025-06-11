b35fd62c29e0ca40111e933717adf2c3cc39ef6d
zengfanxing <zengfanxing@joyy.com>
2025-06-11 17:34:44 +0800

    feat: 新增拦截逻辑

--- scripts/core/fetchInterceptor.core.js
+++ scripts/core/fetchInterceptor.core.js
@@ -7,7 +7,7 @@ function fetchInterceptorInit (w = window) {
             // 调用原始 fetch
             const response = await originalFetch(...args);
             // 你可以在这里处理响应数据
-            const key = response.url?.split('?')?.[0]
+            const key = response.url?.split?.('?')?.[0]
             if (fetchInterceptorMap[key] && typeof fetchInterceptorMap[key] === 'function') {
                 const responseClone = response.clone(); // 克隆响应以便后续处理
                 try {
